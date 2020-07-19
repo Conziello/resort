29ce464112c56a74c1bea806c9e3e156b054fbac
Conziello <conziello@gmail.com>
2020-07-19 13:29:14 -0700

    addition of the data.js file

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Banner  from "../components/Banner";
-import {Link} from 'react-router-dom';
+import Banner  from "../components/Banner"
+import {Link} from 'react-router-dom'
 
 export default function Error(){
-return <Hero>
+return (<Hero>
 
-    <Banner title="404" subtitle="page not founud">
+    <Banner title="404" subtitle="page not found">
         <Link to = '/' className="btn-primary">
            RETURN HOME
         </Link>
     </Banner>
-</Hero>;
+</Hero>);
 }
